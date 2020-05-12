cf0680f8d761d0b4b5bd6a502c166a86b6d0fdac
David Kalbermatten <david.kalbermatten@hotmail.ch>
2020-05-12 10:16:19 +0200

    made executable JavaFX Application!

--- src/main/java/ch/invictech/vehicleManagement/Launch.java
+++ src/main/java/ch/invictech/vehicleManagement/Launch.java
@@ -1,6 +1,7 @@
 package ch.invictech.vehicleManagement;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -10,32 +11,47 @@ import ch.invictech.vehicleManagement.services.PersistenceService;
 import ch.invictech.vehicleManagement.services.RentalService;
 import ch.invictech.vehicleManagement.services.VehicleService;
 
-public class Main extends Application {
-
+public class Launch extends Application {
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void init()
+    {
+        System.out.printf("init() called on thread %s%n",
+                Thread.currentThread());
         GlobalVars.vService = new VehicleService();
         GlobalVars.rService = new RentalService();
         GlobalVars.pService = new PersistenceService();
+        GlobalVars.pService.readFile();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
         GlobalVars.stage = primaryStage;
 
-        GlobalVars.pService.readFile();
-
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/ch/invictech/vehicleManagement/ui//mainScreen/mainScreen.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/ch/invictech/vehicleManagement/ui/mainScreen/mainScreen.fxml"));
         loader.load();
         Parent root = loader.getRoot();
+
         primaryStage.setTitle("Vehicle Management");
         primaryStage.setIconified(false);
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
         primaryStage.setOnCloseRequest(event -> {
             GlobalVars.pService.writeFile();
-            System.exit(0);
+            Platform.exit();
         });
+    }
+
+    @Override
+    public void stop()
+    {
+        System.out.printf("stop() called on thread %s%n",
+                Thread.currentThread());
     }
 
 
     public static void main(String[] args) {
-        launch(args);
+        System.out.printf("main() called on %s%n", Thread.currentThread());
+        Application.launch(args);
+        System.out.print("terminating");
     }
 }
